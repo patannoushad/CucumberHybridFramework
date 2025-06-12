1049cf55f26e03fbb1ae39c933568e3c28bcd87d
noushadpathan1 <noushad.pathan@eigital.com>
2025-06-12 08:56:23 +0530

    file separator

--- src/main/java/utils/TestCaseFilter.java
+++ src/main/java/utils/TestCaseFilter.java
@@ -1,11 +1,12 @@
 package utils;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
 public class TestCaseFilter {
 
-    private static final List<Map<String, String>> testCases = ExcelReader.readExcel(System.getProperty("user.dir")+"\\src\\main\\resources\\testData\\TestCases.xlsx");
+    private static final List<Map<String, String>> testCases = ExcelReader.readExcel(System.getProperty("user.dir") +File.separator+"src" + File.separator + "main" + File.separator + "resources" +File.separator + "testData" + File.separator + "TestCases.xlsx");
 
     public static boolean shouldRunScenario(String scenarioName) {
         return testCases.stream()
